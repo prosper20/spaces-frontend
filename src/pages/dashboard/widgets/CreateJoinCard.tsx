206b27e04d575093c9245e4a341faa635fb1d85a
Jason Dagana <j.dagana@teflonhub.com>
2025-04-20 05:06:09 +0100

    edited

--- src/pages/dashboard/widgets/CreateJoinCard.tsx
+++ src/pages/dashboard/widgets/CreateJoinCard.tsx
@@ -1,30 +1,25 @@
 const plus = "Plus.svg";
 
 const CreateJoinCard = () => (
-	<div className="rounded-[5px] bg-background-primary w-[328px] h-[232px] shadow-box-shadow1 pt-[32px] pr-[31px]">
+	<div className="rounded-[5px] bg-background-primary w-[328px] h-[232px] shadow-box-shadow1 pt-[32px]">
 		<header className="mb-4">
 			<h3 className="text-[24px] font-header1 px-[16px] text-text-100 border-b">
 				Create / Join Group
 			</h3>
 		</header>
 
-		<div className="flex justify-end items-end mt-[60px]">
+		<div className="flex justify-end items-end mt-[60px] pr-[31px]">
 			<button
 				aria-label="create or join"
 				className="
         w-[82px] h-[82px] self-center rounded-full shadow-joincircle bg-[#E7B2A6]
-        text-text-100 hover:bg-[#d89c94] flex justify-center items-center transition
+        text-text-100 hover:bg-[#d89c94] flex justify-center items-center ease-in-out duration-700 transition
       "
 			>
-				<img src={plus} alt="image of plus" className="mt-7 w-[40px]" />
+				<img src={plus} alt="image of plus" className="mt-6 w-[40px]" />
 			</button>
 		</div>
 	</div>
 );
 
 export default CreateJoinCard;
-// On click
-// Navigate to: "Create / Join Group";
-// Animate: Dissolve;
-// animation-timing-function: ease-in-out;
-// animation-duration: 700ms;
