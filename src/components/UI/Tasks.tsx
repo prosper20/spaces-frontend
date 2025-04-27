a7bc098e3ab9b1fd04bf54b501cbb1bae1719749
Jason Dagana <j.dagana@teflonhub.com>
2025-04-27 17:35:17 +0100

    fixed add Task responsiveness

--- src/components/UI/Tasks.tsx
+++ src/components/UI/Tasks.tsx
@@ -40,17 +40,22 @@ const Tasks: React.FC<Props> = () => {
 	}
 	return (
 		<div>
-			<h2 className="text-[36px] font-header text-border mb-6">Add Task</h2>
+			<h2 className="text-[18px] md:text-[36px] font-header text-border md:mb-6">
+				Add Task
+			</h2>
 			<hr className="border-b border-black/[60%] mb-9" />
 
-			<form onSubmit={handleSubmit} className="flex flex-col gap-20 space-y-4">
-				<div className="flex justify-between ">
+			<form
+				onSubmit={handleSubmit}
+				className="flex flex-col gap-4 md:gap-20 space-y-4"
+			>
+				<div className="flex flex-col gap-4 md:gap-0 md:flex-row justify-between ">
 					<Input label={"Title"} name={"title"} />
 
 					<Input label={"Assign to"} name={"assignTo"} />
 				</div>
 
-				<div className="flex justify-between">
+				<div className="flex flex-col gap-4 md:gap-0 md:flex-row justify-between">
 					<Input label={"Description"} name={"description"} />
 					<Input label={"Due date"} name={"dueDate"} />
 				</div>
@@ -58,7 +63,7 @@ const Tasks: React.FC<Props> = () => {
 				<div className="flex justify-end">
 					<button
 						type="submit"
-						className="mt-4 rounded-full border border-brown-800 text-brown-800 font-header shadow-add-task border-border/[40%] bg-background-100 px-8  text-border w-[178px] h-[52px] hover:bg-brown-100 transition text-[32px] "
+						className="mt-4 rounded-full border border-brown-800 text-brown-800 font-header shadow-add-task border-border/[40%] bg-background-100 px-8  text-border  md:w-[178px] md:h-[52px] hover:bg-brown-100 transition text-[16px] md:text-[32px] "
 					>
 						Add
 					</button>
